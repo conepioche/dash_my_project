a11b442156be52d109c6b2903f80b0a420af9a58
conepioche <143957655+conepioche@users.noreply.github.com>
2024-06-23 16:48:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from components import layout
 
 def main():
     app = Dash(__name__)
-    server = app.server
     app.title = "Les Radars"
     app.layout = [
         layout.create_layout(app)
     ]
-    app.run()
+    app.run_server()
 
 
 if __name__ == '__main__':
