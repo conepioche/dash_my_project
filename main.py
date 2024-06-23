83ad1249fd6fdc2f9e85da7b1cdd71f55c5b6eec
conepioche <143957655+conepioche@users.noreply.github.com>
2024-06-23 20:04:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from dash import Dash
-from flask import Flask
 from components import layout
-server = Flask(__name__)
+
+app = Dash(__name__)
+server = app.server
+          
 def main():
-    app = Dash(__name__,server)
     app.title = "Les Radars"
     app.layout = [
         layout.create_layout(app)
     ]
-    app.run_server()
+    app.run()
 
 
 if __name__ == '__main__':
